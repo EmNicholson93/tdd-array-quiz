748a3ecce2e46b83044c5f96dd0ae46641e21a48
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-08 14:09:22 -0700

    still not working but making progress

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,12 @@
 const dontAskWhy = require('..lib/index');
 
-dontAskWhy();
+describe('return strings for words in an array without a specific letter', () => {
+  it('returns string', () => {
+    const wordArray = ['sky', 'hot', 'Yeti', 'green'];
+    dontAskWhy(wordArray);
+    const word = 'hot';
+
+    expect(expect).toequal(`is it ${word}`);
+  });
+});
+
